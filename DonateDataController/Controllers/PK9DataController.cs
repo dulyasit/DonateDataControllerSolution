7ed7b4eb4f2fb86c08f2853881a0c847efa5d72f
dulyasit <dulyasit@hotmail.com>
2018-12-06 21:08:43 +0700

    ปรับปรุงการบันทึกข้อมูล PK9

--- DonateDataController/Controllers/PK9DataController.cs
+++ DonateDataController/Controllers/PK9DataController.cs
@@ -43,14 +43,14 @@ namespace DonateDataController.Controllers
                     docNum = utilLibs.GetDocumentNo("pk9");
                     qExe.Parameters.AddWithValue("@documentbooknumber", docNum.BookNo);
                     qExe.Parameters.AddWithValue("@documentnumber", docNum.DocumentNo);
-                    qExe.Parameters.AddWithValue("@documentdate", Convert.ToDateTime(item.DocumentDate, new CultureInfo("en-US")));
+                    qExe.Parameters.AddWithValue("@documentdate", item.DocumentDate);
                     qExe.Parameters.AddWithValue("@PayerType", item.PayerType);
                     qExe.Parameters.AddWithValue("@PayerRunno", item.PayerRunno);
                     qExe.Parameters.AddWithValue("@AsReceiptTo", item.AsReceiptTo);
                     qExe.Parameters.AddWithValue("@AsReceiptToRemark", item.AsReceiptToRemark);
                     qExe.Parameters.AddWithValue("@ReceiptAmount", item.ReceiptAmount);
                     qExe.Parameters.AddWithValue("@ReceiptPaytype", item.ReceiptPayType);
-                    qExe.Parameters.AddWithValue("@ReceiptDate", Convert.ToDateTime(item.ReceiptDate, new CultureInfo("en-US")));
+                    qExe.Parameters.AddWithValue("@ReceiptDate", item.ReceiptDate);
                     qExe.Parameters.AddWithValue("@ReceiptBank", item.ReceiptBank);
                     qExe.Parameters.AddWithValue("@ReceiptBillNumber", item.ReceiptBillNumber);
                     qExe.Parameters.AddWithValue("@ReceiptChqBank", item.ReceiptChqBank);
@@ -160,6 +160,7 @@ namespace DonateDataController.Controllers
         public IHttpActionResult PK9ListAll()
         {
             UtilLibs utilLibs = new UtilLibs();
+            MasterData masterData = GetMasterData();
             List<ReceiptDataModel> result = new List<ReceiptDataModel>();
             DBConnector.DBConnector conn = new DBConnector.DBConnector();
             if (conn.OpenConnection())
@@ -201,12 +202,32 @@ namespace DonateDataController.Controllers
                     MySqlDataReader dataReader = qExe.ExecuteReader();
                     while (dataReader.Read())
                     {
+                        string CashFlag = "";
+                        string BankFlag = "";
+                        string BillFlag = "";
+                        string ChqFlag = "";
+                        string OtherFlag = "";
+                        if (string.IsNullOrEmpty(dataReader["ReceiptBank"].ToString()) &&
+                            string.IsNullOrEmpty(dataReader["ReceiptBillNumber"].ToString()) &&
+                            string.IsNullOrEmpty(dataReader["ReceiptChqBank"].ToString()) &&
+                            string.IsNullOrEmpty(dataReader["ReceiptOther"].ToString()))
+                            CashFlag = "Y";
+                        else if (!string.IsNullOrEmpty(dataReader["ReceiptBank"].ToString()) &&
+                                 string.IsNullOrEmpty(dataReader["ReceiptChqBank"].ToString()))
+                            BankFlag = "Y";
+                        else if (!string.IsNullOrEmpty(dataReader["ReceiptBillNumber"].ToString()))
+                            BillFlag = "Y";
+                        else if (!string.IsNullOrEmpty(dataReader["ReceiptChqBank"].ToString()))
+                            ChqFlag = "Y";
+                        else
+                            OtherFlag = "Y";
                         ReceiptDataModel detail = new ReceiptDataModel
                         {
                             DocumentRunno = int.Parse(dataReader["documentrunno"].ToString()),
                             DocumentBookNumber = dataReader["documentbooknumber"].ToString(),
                             DocumentNumber = dataReader["documentnumber"].ToString(),
                             DocumentDate = Convert.ToDateTime(dataReader["DocumentDate"].ToString(), new CultureInfo("en-US")),
+                            DocumentDateStr = Convert.ToDateTime(dataReader["DocumentDate"].ToString()).ToString("dd/MM/yyyy"),
                             PayerType = dataReader["PayerType"].ToString(),
                             PayerRunno = int.Parse(dataReader["PayerRunno"].ToString()),
                             AsReceiptTo = dataReader["AsReceiptTo"].ToString(),
@@ -223,9 +244,10 @@ namespace DonateDataController.Controllers
                             Zipcode = dataReader["Zipcode"].ToString(),
                             Telephone = dataReader["Telephone"].ToString(),
                             PayerId = dataReader["PayerID"].ToString(),
-                            PayerName = dataReader["PreName"].ToString() + dataReader["Name"].ToString() + "   " + dataReader["SurName"].ToString(),
+                            PayerName = dataReader["PreName"] + dataReader["Name"].ToString() + "   " + dataReader["SurName"],
                             ReceiptPayType = dataReader["ReceiptPayType"].ToString(),
                             ReceiptDate = Convert.ToDateTime(dataReader["ReceiptDate"].ToString(), new CultureInfo("en-US")),
+                            ReceiptDateStr = Convert.ToDateTime(dataReader["ReceiptDate"].ToString()).ToString("dd/MM/yyyy"),
                             ReceiptBank = dataReader["ReceiptBank"].ToString(),
                             BankName = utilLibs.GetBankName(dataReader["ReceiptBank"].ToString()),
                             ReceiptBillNumber = dataReader["ReceiptBillNumber"].ToString(),
@@ -233,7 +255,17 @@ namespace DonateDataController.Controllers
                             ReceiptChqNumber = dataReader["ReceiptChqNumber"].ToString(),
                             ReceiptOther = dataReader["ReceiptOther"].ToString(),
                             CitizenId = dataReader["CitizenID"].ToString(),
-                            AmountStr = utilLibs.ThaiBaht(dataReader["ReceiptAmount"].ToString())
+                            AmountStr = utilLibs.ThaiBaht(dataReader["ReceiptAmount"].ToString()),
+                            PartyName = masterData.PartyName,
+                            PartyTel = masterData.Telephone,
+                            PartyTaxID = "เลขประจำตัวผู้เสียภาษี " + masterData.TaxID,
+                            PartyAddr1 = GetAddrRow1(masterData),
+                            PartyAddr2 = GetAddrRow2(masterData),
+                            CashFlag = CashFlag,
+                            BankFlag = BankFlag,
+                            BillFlag = BillFlag,
+                            ChqFlag = ChqFlag,
+                            OtherFlag = OtherFlag
                         };
                         result.Add(detail);
                     }
@@ -251,12 +283,13 @@ namespace DonateDataController.Controllers
                 return BadRequest("Database connect fail!");
             }
         }
-        
+
         [Route("PK9Data/GetByID/{id}")]
         [HttpGet]
         public IHttpActionResult PK9GetByID(string id)
         {
             UtilLibs utilLibs = new UtilLibs();
+            MasterData masterData = GetMasterData();
             ReceiptDataModel result = new ReceiptDataModel();
             DBConnector.DBConnector conn = new DBConnector.DBConnector();
             if (conn.OpenConnection())
@@ -301,10 +334,30 @@ namespace DonateDataController.Controllers
                     MySqlDataReader dataReader = qExe.ExecuteReader();
                     while (dataReader.Read())
                     {
+                        string CashFlag = "";
+                        string BankFlag = "";
+                        string BillFlag = "";
+                        string ChqFlag = "";
+                        string OtherFlag = "";
+                        if (string.IsNullOrEmpty(dataReader["ReceiptBank"].ToString()) &&
+                            string.IsNullOrEmpty(dataReader["ReceiptBillNumber"].ToString()) &&
+                            string.IsNullOrEmpty(dataReader["ReceiptChqBank"].ToString()) &&
+                            string.IsNullOrEmpty(dataReader["ReceiptOther"].ToString()))
+                            CashFlag = "Y";
+                        else if (!string.IsNullOrEmpty(dataReader["ReceiptBank"].ToString()) &&
+                                 string.IsNullOrEmpty(dataReader["ReceiptChqBank"].ToString()))
+                            BankFlag = "Y";
+                        else if (!string.IsNullOrEmpty(dataReader["ReceiptBillNumber"].ToString()))
+                            BillFlag = "Y";
+                        else if (!string.IsNullOrEmpty(dataReader["ReceiptChqBank"].ToString()))
+                            ChqFlag = "Y";
+                        else
+                            OtherFlag = "Y";
                         result.DocumentRunno = int.Parse(dataReader["documentrunno"].ToString());
                         result.DocumentBookNumber = dataReader["documentbooknumber"].ToString();
                         result.DocumentNumber = dataReader["documentnumber"].ToString();
                         result.DocumentDate = Convert.ToDateTime(dataReader["documentdate"].ToString(), new CultureInfo("en-US"));
+                        result.DocumentDateStr = Convert.ToDateTime(dataReader["DocumentDate"].ToString()).ToString("dd/MM/yyyy");
                         result.PayerType = dataReader["PayerType"].ToString();
                         result.PayerRunno = int.Parse(dataReader["PayerRunno"].ToString());
                         result.AsReceiptTo = dataReader["AsReceiptTo"].ToString();
@@ -324,6 +377,7 @@ namespace DonateDataController.Controllers
                         result.PayerName = dataReader["PreName"].ToString() + dataReader["Name"].ToString() + "   " + dataReader["SurName"].ToString();
                         result.ReceiptPayType = dataReader["ReceiptPayType"].ToString();
                         result.ReceiptDate = Convert.ToDateTime(dataReader["ReceiptDate"].ToString(), new CultureInfo("en-US"));
+                        result.ReceiptDateStr = Convert.ToDateTime(dataReader["ReceiptDate"].ToString()).ToString("dd/MM/yyyy");
                         result.ReceiptBank = dataReader["ReceiptBank"].ToString();
                         result.BankName = utilLibs.GetBankName(dataReader["ReceiptBank"].ToString());
                         result.ReceiptBillNumber = dataReader["ReceiptBillNumber"].ToString();
@@ -332,6 +386,16 @@ namespace DonateDataController.Controllers
                         result.ReceiptOther = dataReader["ReceiptOther"].ToString();
                         result.CitizenId = dataReader["CitizenID"].ToString();
                         result.AmountStr = utilLibs.ThaiBaht(dataReader["ReceiptAmount"].ToString());
+                        result.PartyName = masterData.PartyName;
+                        result.PartyTel = masterData.Telephone;
+                        result.PartyTaxID = "เลขประจำตัวผู้เสียภาษี " + masterData.TaxID;
+                        result.PartyAddr1 = GetAddrRow1(masterData);
+                        result.PartyAddr2 = GetAddrRow2(masterData);
+                        result.CashFlag = CashFlag;
+                        result.BankFlag = BankFlag;
+                        result.BillFlag = BillFlag;
+                        result.ChqFlag = ChqFlag;
+                        result.OtherFlag = OtherFlag;
                     }
                     dataReader.Close();
                     conn.CloseConnection();
@@ -346,6 +410,78 @@ namespace DonateDataController.Controllers
             {
                 return BadRequest("Database connect fail!");
             }
+        }
+        private MasterData GetMasterData()
+        {
+            MasterData result = new MasterData();
+            DBConnector.DBConnector conn = new DBConnector.DBConnector();
+            string SQLString;
+            if (conn.OpenConnection())
+            {
+                SQLString = @"select * from masterdata";
+                MySqlCommand qExe = new MySqlCommand
+                {
+                    Connection = conn.connection,
+                    CommandText = SQLString
+                };
+                MySqlDataReader dataReader = qExe.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    result = new MasterData
+                    {
+                        PartyName = dataReader["partyname"].ToString(),
+                        HouseNumber = dataReader["housenumber"].ToString(),
+                        Soi = dataReader["soi"].ToString(),
+                        Road = dataReader["road"].ToString(),
+                        Moo = dataReader["moo"].ToString(),
+                        Building = dataReader["building"].ToString(),
+                        Tambon = dataReader["tambon"].ToString(),
+                        Amphur = dataReader["amphur"].ToString(),
+                        Province = dataReader["province"].ToString(),
+                        Zipcode = dataReader["zipcode"].ToString(),
+                        Telephone = dataReader["telephone"].ToString(),
+                        Slogan = dataReader["Slogan"].ToString(),
+                        TaxID = dataReader["TaxID"].ToString()
+                    };
+                }
+            }
+            return result;
+        }
+        private string GetAddrRow1(MasterData masterData)
+        {
+            string result = "";
+            if (!string.IsNullOrEmpty(masterData.HouseNumber))
+                result = "เลขที่ " + masterData.HouseNumber;
+            if (!string.IsNullOrEmpty(masterData.Moo))
+                result = result + " หมู่ที่ " + masterData.Moo;
+            if (!string.IsNullOrEmpty(masterData.Building))
+                result = result + " อาคาร " + masterData.Building;
+            if (!string.IsNullOrEmpty(masterData.Soi))
+                result = result + " ซอย " + masterData.Soi;
+            if (!string.IsNullOrEmpty(masterData.Road))
+                result = result + " ถนน " + masterData.Road;
+            if (!string.IsNullOrEmpty(masterData.Tambon))
+                if (masterData.Province.Contains("กรุงเทพ") || masterData.Province.Contains("กทม"))
+                    result = result + " แขวง " + masterData.Tambon;
+                else
+                    result = result + " ตำบล " + masterData.Tambon;
+            return result.Trim();
+        }
+        private string GetAddrRow2(MasterData masterData)
+        {
+            string result = "";
+            if (!string.IsNullOrEmpty(masterData.Amphur))
+                if (masterData.Province.Contains("กรุงเทพ") || masterData.Province.Contains("กทม"))
+                    result = "เขต " + masterData.Amphur;
+                else
+                    result = "อำเภอ " + masterData.Amphur;
+            if (!string.IsNullOrEmpty(masterData.Province))
+                result = result + " จังหวัด " + masterData.Province;
+            if (!string.IsNullOrEmpty(masterData.Zipcode))
+                result = result + " " + masterData.Zipcode;
+            if (!string.IsNullOrEmpty(masterData.Telephone))
+                result = result + " โทร. " + masterData.Telephone;
+            return result.Trim();
         }
     }
 }
